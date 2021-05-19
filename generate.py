d25b397b160d80f53177694cec3439065413d8cb
Stefan Garlonta <stefan.garlonta@gmail.com>
2021-05-19 13:27:29 +0000

    Update Readme

--- generate.py
+++ generate.py
@@ -3,5 +3,5 @@ import os
 if __name__ == "__main__":
     with open("README.md", "a") as readme:
         for i in os.listdir("buttons/150x44/"):
-            readme.write("[![Button](https://raw.githubusercontent.com/garlontas/buttons/main/buttons/150x44/{0})](https://github.com/garlontas/buttons#readme)".format(i))
+            readme.write("[![Button](https://raw.githubusercontent.com/garlontas/buttons/main/buttons/150x44/{0})](https://github.com/garlontas/buttons#svg-buttons)".format(i))
             readme.write("\n")
